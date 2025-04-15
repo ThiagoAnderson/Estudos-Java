436ca00a092e5dfb0505d331c7caff829b49cc29
Thiago Anderson <thiago.andersom.silva@hotmail.com>
2025-04-15 00:08:05 -0300

    Errors,Exceptions,Checked,RuntimeExceptions

---
+++ src/academy/devdojo/maratonajava/javacore/OException/runtime/test/RuntimeExceptionTest01.java
@@ -0,0 +1,9 @@
+package academy.devdojo.maratonajava.javacore.OException.runtime.test;
+
+public class RuntimeExceptionTest01 {
+    public static void main(String[] args) {
+        // Checked e Uncheckd
+        Object obj = null;
+        System.out.println(obj.toString());
+    }
+}
